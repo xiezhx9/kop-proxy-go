16c3de7f3ebd6ea5f05f8daf695b440c21ecc355
ZhangJian He <shoothzj@gmail.com>
2023-04-03 17:38:33 +0800

    Update kafka codec go (#6)

--- go.mod
+++ go.mod
@@ -7,11 +7,11 @@ require (
 	github.com/gogo/protobuf v1.3.2
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
-	github.com/protocol-laboratory/kafka-codec-go v0.0.0-20230307115521-31a0d51d09b4
+	github.com/protocol-laboratory/kafka-codec-go v0.0.0-20230403092950-13d385dd3ebd
 	github.com/protocol-laboratory/pulsar-admin-go v0.0.0-20230307111555-33b695a0a108
 	github.com/protocol-laboratory/pulsar-codec-go v0.0.0-20221012004921-e001bc05e13a
 	github.com/sirupsen/logrus v1.9.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.2
 )
 
 require (
@@ -61,7 +61,7 @@ require (
 	go.uber.org/atomic v1.10.0 // indirect
 	golang.org/x/net v0.0.0-20220906165146-f3363e06e74c // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
--- kop/kop_impl.go
+++ kop/kop_impl.go
@@ -2,110 +2,110 @@ package kop
 
 import (
 	"github.com/protocol-laboratory/kafka-codec-go/codec"
-	"net"
+	"github.com/protocol-laboratory/kafka-codec-go/knet"
 )
 
-func (b *Broker) ConnectionClosed(conn net.Conn) {
+func (b *Broker) ConnectionClosed(conn *knet.Conn) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) AcceptError(conn net.Conn, err error) {
+func (b *Broker) AcceptError(conn *knet.Conn, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) ReadError(conn net.Conn, err error) {
+func (b *Broker) ReadError(conn *knet.Conn, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) ReactError(conn net.Conn, err error) {
+func (b *Broker) ReactError(conn *knet.Conn, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) WriteError(conn net.Conn, err error) {
+func (b *Broker) WriteError(conn *knet.Conn, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) UnSupportedApi(conn net.Conn, apiKey codec.ApiCode, apiVersion int16) {
+func (b *Broker) UnSupportedApi(conn *knet.Conn, apiKey codec.ApiCode, apiVersion int16) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) ApiVersion(conn net.Conn, req *codec.ApiReq) (*codec.ApiResp, error) {
+func (b *Broker) ApiVersion(conn *knet.Conn, req *codec.ApiReq) (*codec.ApiResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) Fetch(conn net.Conn, req *codec.FetchReq) (*codec.FetchResp, error) {
+func (b *Broker) Fetch(conn *knet.Conn, req *codec.FetchReq) (*codec.FetchResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) FindCoordinator(conn net.Conn, req *codec.FindCoordinatorReq) (*codec.FindCoordinatorResp, error) {
+func (b *Broker) FindCoordinator(conn *knet.Conn, req *codec.FindCoordinatorReq) (*codec.FindCoordinatorResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) Heartbeat(conn net.Conn, req *codec.HeartbeatReq) (*codec.HeartbeatResp, error) {
+func (b *Broker) Heartbeat(conn *knet.Conn, req *codec.HeartbeatReq) (*codec.HeartbeatResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) JoinGroup(conn net.Conn, req *codec.JoinGroupReq) (*codec.JoinGroupResp, error) {
+func (b *Broker) JoinGroup(conn *knet.Conn, req *codec.JoinGroupReq) (*codec.JoinGroupResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) LeaveGroup(conn net.Conn, req *codec.LeaveGroupReq) (*codec.LeaveGroupResp, error) {
+func (b *Broker) LeaveGroup(conn *knet.Conn, req *codec.LeaveGroupReq) (*codec.LeaveGroupResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) ListOffsets(conn net.Conn, req *codec.ListOffsetsReq) (*codec.ListOffsetsResp, error) {
+func (b *Broker) ListOffsets(conn *knet.Conn, req *codec.ListOffsetsReq) (*codec.ListOffsetsResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) Metadata(conn net.Conn, req *codec.MetadataReq) (*codec.MetadataResp, error) {
+func (b *Broker) Metadata(conn *knet.Conn, req *codec.MetadataReq) (*codec.MetadataResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) OffsetCommit(conn net.Conn, req *codec.OffsetCommitReq) (*codec.OffsetCommitResp, error) {
+func (b *Broker) OffsetCommit(conn *knet.Conn, req *codec.OffsetCommitReq) (*codec.OffsetCommitResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) OffsetFetch(conn net.Conn, req *codec.OffsetFetchReq) (*codec.OffsetFetchResp, error) {
+func (b *Broker) OffsetFetch(conn *knet.Conn, req *codec.OffsetFetchReq) (*codec.OffsetFetchResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) OffsetForLeaderEpoch(conn net.Conn, req *codec.OffsetForLeaderEpochReq) (*codec.OffsetForLeaderEpochResp, error) {
+func (b *Broker) OffsetForLeaderEpoch(conn *knet.Conn, req *codec.OffsetForLeaderEpochReq) (*codec.OffsetForLeaderEpochResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) Produce(conn net.Conn, req *codec.ProduceReq) (*codec.ProduceResp, error) {
+func (b *Broker) Produce(conn *knet.Conn, req *codec.ProduceReq) (*codec.ProduceResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) SaslAuthenticate(conn net.Conn, req *codec.SaslAuthenticateReq) (*codec.SaslAuthenticateResp, error) {
+func (b *Broker) SaslAuthenticate(conn *knet.Conn, req *codec.SaslAuthenticateReq) (*codec.SaslAuthenticateResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) SaslHandshake(conn net.Conn, req *codec.SaslHandshakeReq) (*codec.SaslHandshakeResp, error) {
+func (b *Broker) SaslHandshake(conn *knet.Conn, req *codec.SaslHandshakeReq) (*codec.SaslHandshakeResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (b *Broker) SyncGroup(conn net.Conn, req *codec.SyncGroupReq) (*codec.SyncGroupResp, error) {
+func (b *Broker) SyncGroup(conn *knet.Conn, req *codec.SyncGroupReq) (*codec.SyncGroupResp, error) {
 	//TODO implement me
 	panic("implement me")
 }
