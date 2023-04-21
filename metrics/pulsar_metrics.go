15dbb7597718e50a6ab1742f0ea3559bd174ae07
ZhangJian He <shoothzj@gmail.com>
2023-04-21 16:55:57 +0800

    correct metrics name

--- metrics/pulsar_metrics.go
+++ metrics/pulsar_metrics.go
@@ -8,30 +8,30 @@ import (
 var (
 	PulsarSendSuccessCount = promauto.NewCounter(
 		prometheus.CounterOpts{
-			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_success_total")},
+			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_send_success_total")},
 	)
 	PulsarSendFailCount = promauto.NewCounter(
 		prometheus.CounterOpts{
-			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_fail_total")},
+			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_send_fail_total")},
 	)
 	PulsarSendLatency = promauto.NewSummary(
 		prometheus.SummaryOpts{
-			Name:       prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_latency_ms"),
+			Name:       prometheus.BuildFQName(namespace, "pulsar", "produce_send_latency_ms"),
 			Objectives: objectives},
 	)
 	PulsarTopicSendSuccessCount = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_send_success_total")},
+			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_success_total")},
 		[]string{"kafka_topic", "pulsar_topic"},
 	)
 	PulsarTopicSendFailCount = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_send_fail_total")},
+			Name: prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_fail_total")},
 		[]string{"kafka_topic", "pulsar_topic"},
 	)
 	PulsarTopicSendLatency = promauto.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Name:       prometheus.BuildFQName(namespace, "pulsar", "produce_send_latency_ms"),
+			Name:       prometheus.BuildFQName(namespace, "pulsar", "produce_topic_send_latency_ms"),
 			Objectives: objectives},
 		[]string{"kafka_topic", "pulsar_topic"},
 	)
